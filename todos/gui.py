e644f5072559a20d508db6ac75ccf65c801911b8
jlarryc <jlarryc@yahoo.com>
2024-01-11 06:44:14 -0600

    Implement checked, delete and exit buttons

--- todos/gui.py
+++ todos/gui.py
@@ -9,8 +9,19 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 
+checkoff_button = sg.Button("Checkoff")
+
+delete_button = sg.Button("Delete")
+
+exit_button = sg.Button("Exit")
+
+
+
 window = sg.Window("ToDo App With a Very Long Title",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, checkoff_button, delete_button],
+                           [exit_button]],
                    font = ("Helvetica", 16))
 while True:
     event, values = window.read()
@@ -27,6 +38,16 @@ while True:
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
+            new_todo = new_todo
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Checkoff":
+            todo_to_edit = values['todos'][0]
+            new_todo = f"|X| {values['todo']}"
             todos = functions.get_todos()
 
             # values[todo_to_edit] = new_todo
@@ -35,9 +56,18 @@ while True:
 
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Delete":
+            todo_to_delete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_delete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update("")
         case 'todos':
             values['todo'] = values['todos']
             window['todo'].update(value=values['todos'][0])
+        case "Exit":
+            break
         case sg.WIN_CLOSED:  # WIN_CLOSED is defined as None
             break
 
